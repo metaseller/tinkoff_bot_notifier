46e20c327e5ee386f9ce81c9fb2d09c518ff162f
Ilya <ilya-m9@yandex.ru>
2021-06-19 19:59:19 +0700

    first commit

--- app/controllers/TinkoffController.php
+++ app/controllers/TinkoffController.php
@@ -1,9 +1,29 @@
 <?php
-
 namespace app\controllers;
 use yii\console\Controller;
+use \jamesRUS52\TinkoffInvest\TIClient;
+use \jamesRUS52\TinkoffInvest\TISiteEnum;
+use \jamesRUS52\TinkoffInvest\TICurrencyEnum;
+use \jamesRUS52\TinkoffInvest\TIInstrument;
+use \jamesRUS52\TinkoffInvest\TIPortfolio;
+use \jamesRUS52\TinkoffInvest\TIOperationEnum;
+use \jamesRUS52\TinkoffInvest\TIIntervalEnum;
+use \jamesRUS52\TinkoffInvest\TICandleIntervalEnum;
+use \jamesRUS52\TinkoffInvest\TICandle;
+use \jamesRUS52\TinkoffInvest\TIOrderBook;
+use \jamesRUS52\TinkoffInvest\TIInstrumentInfo;
+
+define("TOKEN",     "t.XtUKvAmIkeXWviB7brsoJ-2VAQEg4FAcKmDumDltzrnGZJbPFgBsZjYxYiokXYojR7haUBCjUwjWRHew6EWZnw");
 
 class TinkoffController extends Controller
 {
+    //Инициализация юзера, добавление в БД;
+    public static function ActionInitUser(){
+    }
 
+    public static function ActionAddTicker(){
+
+    }
+    public static function  ActionCheckStocks(){
+    }
 }
