873bd8347c63bc61fe4043e49e3e6a84c4226eb6
Ilya <ilya-m9@yandex.ru>
2021-06-19 19:57:55 +0700

    first commit

--- app/commands/BotController.php
+++ app/commands/BotController.php
@@ -5,14 +5,6 @@ namespace app\commands;
 use yii\console\Controller;
 use app\controllers\TelegramController;
 
-/**
- * This command echoes the first argument that you have entered.
- *
- * This command is provided as an example for you to learn how to create console commands.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class BotController extends Controller
 {
     public function actionStart()
