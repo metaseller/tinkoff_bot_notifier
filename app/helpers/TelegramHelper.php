3e791f489c3738a88e5c8e006d2e059a9853ea3f
Ilya <ilya-m9@yandex.ru>
2021-07-02 16:47:11 +0700

    basic command checking

--- app/helpers/TelegramHelper.php
+++ app/helpers/TelegramHelper.php
@@ -7,8 +7,22 @@ class TelegramHelper
     private $lastMessageId = 0;
     private $botToken = "1773680987:AAG8q5LPSzxkA8g502jBEiPp1s-O0GWKTfU";
 
+    public function getCommand() {
 
-    public function sendMessage($textMessage = "This is my message !!!", $chatId = 442912517){
+        $message = $this->getUpdates();
+        if ($message != null) {
+            if (substr($message['text'], 0, 1) === '/') {
+                preg_match_all('/(\w+)/', $message['text'], $matches);
+                $command_name = $matches[0][0];
+                $parameters = array_slice($matches[0], 1);
+                return ['id_telegram' => $message['id_telegram'], 'command_name' => $command_name, 'parameters' => $parameters];
+            }
+        }
+
+    }
+
+    public function sendMessage($textMessage = "This is my message !!!", $chatId = 442912517) {
+
         $website = "https://api.telegram.org/bot" . $this->botToken;
         $params = [
             'chat_id' => $chatId,
@@ -24,11 +38,9 @@ class TelegramHelper
 
     }
 
-    public function getUpdates(){
-        echo "\ngetUpdates\n";
+    public function getUpdates() {
 
         $website = "https://api.telegram.org/bot" . $this->botToken;
-
         $ch = curl_init($website . '/getUpdates?offset=-1');
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -36,34 +48,31 @@ class TelegramHelper
         $result = curl_exec($ch);
         curl_close($ch);
         $result = json_decode($result);
+        if ($result) {
+            $result = $result->result[0];
+            if ($this->isAlreadyViewed($result)) {
+                echo $result->message->text;
+            }
+            else {
+                $this->lastMessageId = $result->update_id;
+                echo $result->message->text;
 
-        $result = $result->result[0];
-
-        var_dump($result);
-        if($this->isAlreadyViewed($result)){
-            echo $result->message->text;
-        }else{
-
-            $this->sendMessage("Сообщение получино и обработано", $result->message->from->id);
-            $this->lastMessageId = $result->update_id;
-            //$this->foundCommands($result->message->text);
-            echo $result->message->text;
-
-            return [
-                    "id_telegram" => $result->message->from->id,
-                    "text" => $result->message->text
-                   ];
+                return [
+                    'id_telegram' => $result->message->from->id,
+                    'text' => $result->message->text
+                ];
+            }
         }
     }
 
-    public function isAlreadyViewed($result){
+    public function isAlreadyViewed($result) {
+
         // todo вытащить из базы данных инфу об этом сообщении, обработали ли мы его когда то?
         // и проверка человека, новый ли он?
 
         return $result->update_id == $this->lastMessageId;
+
     }
-
-
 
 }
 
